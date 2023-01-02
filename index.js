4193ff1c35621c055a90be1b02a1bf0649a5fca3
Garrett <garrettjrh@live.com>
2023-01-02 04:02:27 -0700

    Added event listener for mouse coordinates that move svg shapes in the background of the landing page.

--- index.js
+++ index.js
@@ -2,12 +2,27 @@
 // service_lo59kw2
 // hDCgqlu9cChmWH-zT
 
+let contrastToggle = false;
+let isModalOpen = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+    }
+}
 
 function contact(event) {
     event.preventDefault()
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList += " modal__overlay--visible"
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
     emailjs
         .sendForm(
             'service_lo59kw2',
@@ -15,35 +30,33 @@ function contact(event) {
             event.target,
             'hDCgqlu9cChmWH-zT'
     ).then(() => {
-        loading.classList.remove("modal__overlay--visible")
-        success.classList += " modal__overlay--visible"
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
     }).catch(() => {
-        loading.classList.remove("modal__overlay--visible")
+        loading.classList.remove("modal__overlay--visible");
         alert(
             "The email service is temporarily unavailable. Please contact me directly at garrettjrh@live.com"
         )
     })
 }
 
-let contrastToggle = false;
 function toggleContrast() {
-    contrastToggle = !contrastToggle
+    contrastToggle = !contrastToggle;
     if (contrastToggle) {
-        console.log('toggle on')
-        return document.body.classList += " dark-theme"
+        console.log('toggle on');
+        return document.body.classList += " dark-theme";
     }
     else {
-        console.log('toggle off')
-        return document.body.classList.remove("dark-theme")
+        console.log('toggle off');
+        return document.body.classList.remove("dark-theme");
     }
 }
 
-let isModalOpen = false;
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
-        return document.body.classList.remove("modal--open")
+        return document.body.classList.remove("modal--open");
     }
     isModalOpen = true;
-    document.body.classList += " modal--open"
+    document.body.classList += " modal--open";
 }
